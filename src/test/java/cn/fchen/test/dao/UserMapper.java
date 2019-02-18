0723fe9ed439db59927b3565e4549fc5dc9c9d48
Fchen <fc_2012@163.com>
2019-02-18 19:36:32 +0800

    update

---
+++ src/test/java/cn/fchen/test/dao/UserMapper.java
@@ -0,0 +1,13 @@
+package cn.fchen.test.dao;
+
+import cn.fchen.test.pojo.User;
+
+/**
+ * @Classname UserMapper
+ * @Description TODO
+ * @Date 2019/2/18 18:01
+ * @Author by Fchen
+ */
+public interface UserMapper {
+    User getUser(Integer id);
+}
---
+++ src/test/java/cn/fchen/test/pojo/User.java
@@ -0,0 +1,29 @@
+package cn.fchen.test.pojo;
+
+/**
+ * @Classname User
+ * @Description 对应的用户类
+ * @Date 2019/2/18 17:46
+ * @Author by Fchen
+ */
+
+public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
