faf7d18c8662da17e835e1dee217d4607f99da60
FunCheney <fcfc_2012@163.com>
2019-02-23 09:35:04 +0800

    update md 文档

--- README.md
+++ README.md
@@ -7,5 +7,14 @@
        2.到clone下来的代码对应的pom.xml文件所在的地方，执行 gradle init --type pom 命令，先保证本机安装了gradle 2.0以上的版本
        3.删除原先的pom.xml文件
        4.在build.gradle 中加入 compile group: 'mysql', name: 'mysql-connector-java', version:'5.1.32'
+     测试用例编写：
+        1.在test目录下创建 package -> cn.fchen
+        2.创建sql文件夹用来保存sql语句 sql -> UserTestSql.md 文件。
+        3.编写数据库表‘user_test’ 对应的实体类 user
+        4.编写UserMapper.xml 文件
+        5.创建 resources 目录 
+           在此目录下创建：mybatis-config-local.xml ->mybatis 配置文件
+                       ：mybatis/UserMapper.xml 文件
+        6.编写测试用例：myTest.java 测试结果;
                                         
                                         
--- src/test/java/cn/fchen/myTest.java
+++ src/test/java/cn/fchen/myTest.java
@@ -4,7 +4,10 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -15,19 +18,31 @@ import java.io.InputStream;
  */
 public class myTest {
 
-    public static void main(String[] args) {
-        try {
-            String resource = "mybatis-config-local.xml";
-            InputStream inputStream = Resources.getResourceAsStream(resource);
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-            System.out.println("sqlSessionFactory初始化-->"+sqlSessionFactory);
-            SqlSession session = sqlSessionFactory.openSession();
-            UserMapper mapper = session.getMapper(UserMapper.class);
-            User user = mapper.getUser(1);
-            System.out.println(user.toString());
-            session.close();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+    private SqlSessionFactory sqlSessionFactory = null;
+
+    @Before
+    public void init() throws IOException {
+        // 1.加载核心配置文件（sqlMapConfig.xml）
+        // resource参数：指定配置文件的位置
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config-local.xml");
+
+        // 2.读取配置文件内容
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // sqlSessionFactory对象：mybatis框架的核心对象，通常一个应用中只需要一个（单例设计模式），是线程安全
+        sqlSessionFactory = builder.build(inputStream);
+    }
+
+    // 测试根据用户id查询用户
+    @Test
+    public void queryUserByIdTest() throws IOException{
+
+        SqlSession session = sqlSessionFactory.openSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.getUser(1);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        System.out.println(user);
+
+        // 5.释放资源
+        sqlSession.close();
     }
 }
