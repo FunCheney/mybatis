bbf583212e3d3d99852592866cd36f1e65c2f8df
Fchen <fc_2012@163.com>
2019-03-14 17:15:28 +0800

    test

---
+++ src/test/java/cn/fchen/sqlsessionfactory/LoadXmlTest.java
@@ -0,0 +1,21 @@
+package cn.fchen.sqlsessionfactory;
+
+import cn.fchen.User;
+import cn.fchen.UserMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @Classname LoadXmlTest
+ * @Description 通过xml获取  SqlSessionFactory
+ * @Date 2019/3/14 17:03
+ * @Author by Fchen
+ */
+public class LoadXmlTest {
+
+    @Test
+    public void queryUserByIdTest() throws IOException {
+
+    }
+}
---
+++ src/test/java/cn/fchen/sqlsessionfactory/UseJavaTest.java
@@ -0,0 +1,19 @@
+package cn.fchen.sqlsessionfactory;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @Classname LoadXmlTest
+ * @Description 通过非xml方式获取  SqlSessionFactory
+ * @Date 2019/3/14 17:03
+ * @Author by Fchen
+ */
+public class UseJavaTest {
+
+    @Test
+    public void queryUserByIdTest() throws IOException {
+
+    }
+}
